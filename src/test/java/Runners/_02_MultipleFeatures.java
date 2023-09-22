534a5783e7906484f8531666529f98c04a23e804
Tunahan <tunahanboyaci1@gmail.com>
2023-09-22 14:26:55 -0400

    NewProject20

--- src/test/java/Runners/_02_MultipleFeatures.java
+++ src/test/java/Runners/_02_MultipleFeatures.java
@@ -4,8 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions (
-        features = {"src/test/java/FeatureFiles/_01_Login.feature",
-        "src/test/java/FeatureFiles/_02_Country.feature"},
+        features = "src/test/java/FeatureFiles/_01_Login.feature",
         glue = "StepDefinitions",
         plugin = {"pretty", "html:target/site/cucmber-pretty", "json:target/cucumber/cucumber.json"}
 )
